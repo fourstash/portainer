586e0e10d485366a6e8f92907157ddc910357752
Chaim Lev-Ari <chiptus@gmail.com>
2021-07-15 12:32:02 +0300

    docs(ssl): add api docs

--- api/http/handler/ssl/ssl_inspect.go
+++ api/http/handler/ssl/ssl_inspect.go
@@ -7,6 +7,18 @@ import (
 	"github.com/portainer/libhttp/response"
 )
 
+// @id SSLInspect
+// @summary Inspect the ssl settings
+// @description Retrieve the ssl settings.
+// @description **Access policy**: administrator
+// @tags ssl
+// @security jwt
+// @produce json
+// @success 200 {object} portainer.SSLSettings "Success"
+// @failure 400 "Invalid request"
+// @failure 403 "Permission denied to access settings"
+// @failure 500 "Server error"
+// @router /ssl [get]
 func (handler *Handler) sslInspect(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
 	settings, err := handler.SSLService.GetSSLSettings()
 	if err != nil {
--- api/http/handler/ssl/ssl_update.go
+++ api/http/handler/ssl/ssl_update.go
@@ -23,6 +23,37 @@ func (payload *sslUpdatePayload) Validate(r *http.Request) error {
 	return nil
 }
 
+// @id RegistryUpdate
+// @summary Update a registry
+// @description Update a registry
+// @description **Access policy**: administrator
+// @tags registries
+// @security jwt
+// @accept json
+// @produce json
+// @param id path int true "Registry identifier"
+// @param body body registryUpdatePayload true "Registry details"
+// @success 200 {object} portainer.Registry "Success"
+// @failure 400 "Invalid request"
+// @failure 404 "Registry not found"
+// @failure 409 "Another registry with the same URL already exists"
+// @failure 500 "Server error"
+// @router /registries/{id} [put]
+
+// @id SSLUpdate
+// @summary Update the ssl settings
+// @description Update the ssl settings.
+// @description **Access policy**: administrator
+// @tags ssl
+// @security jwt
+// @accept json
+// @produce json
+// @param body body sslUpdatePayload true "SSL Settings"
+// @success 204 "Success"
+// @failure 400 "Invalid request"
+// @failure 403 "Permission denied to access settings"
+// @failure 500 "Server error"
+// @router /ssl [put]
 func (handler *Handler) sslUpdate(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
 	var payload sslUpdatePayload
 	err := request.DecodeAndValidateJSONPayload(r, &payload)
